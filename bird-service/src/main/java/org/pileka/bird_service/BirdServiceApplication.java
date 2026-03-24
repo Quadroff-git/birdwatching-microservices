d0b69ffe42fe27b3a2f1408de3777f83d328b0d1
Pavel Pileka <pavelpileka@gmail.com>
2026-03-24 19:40:55 +0300

    Initial commit

---
+++ bird-service/src/main/java/org/pileka/bird_service/BirdServiceApplication.java
@@ -0,0 +1,13 @@
+package org.pileka.bird_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdServiceApplication.class, args);
+	}
+
+}
---
+++ bird-service/src/test/java/org/pileka/bird_service/BirdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pileka.bird_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
