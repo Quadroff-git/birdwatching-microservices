57ac5551611730918b72a2f49e2ac08774967dc9
Pavel Pileka <pavelpileka@gmail.com>
2026-03-26 12:02:05 +0300

    feat: add ConservationStatus enum

---
+++ bird-service/src/main/java/org/pileka/bird_service/model/ConservationStatus.java
@@ -0,0 +1,12 @@
+package org.pileka.bird_service.model;
+
+public enum ConservationStatus {
+    LEAST_CONCERN,
+    NEAR_THREATENED,
+    VULNERABLE,
+    ENDANGERED,
+    CRITICALLY_ENDANGERED,
+    EXTINCT_IN_WILD,
+    EXTINCT,
+    DATA_DEFICIENT
+}
