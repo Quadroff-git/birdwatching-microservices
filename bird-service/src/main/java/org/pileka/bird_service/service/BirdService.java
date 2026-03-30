e3508249939f0defebc091fa5ac733f73efbef2c
Pavel Pileka <pavelpileka@gmail.com>
2026-03-30 16:53:21 +0300

    feat: add authorization to BirdService

--- bird-service/src/main/java/org/pileka/bird_service/service/BirdService.java
+++ bird-service/src/main/java/org/pileka/bird_service/service/BirdService.java
@@ -5,18 +5,22 @@ import org.pileka.bird_service.dto.QueryBirdDTO;
 import org.pileka.bird_service.dto.ResponseBirdDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 
 public interface BirdService {
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'RESEARCHER')")
     ResponseBirdDTO create(CreateUpdateBirdDTO createDto);
 
     ResponseBirdDTO getById(long id);
 
     Page<ResponseBirdDTO> get(QueryBirdDTO queryDto, Pageable pageable);
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'RESEARCHER')")
     ResponseBirdDTO update(CreateUpdateBirdDTO updateDto);
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'RESEARCHER')")
     ResponseBirdDTO delete(long id);
 
 }
